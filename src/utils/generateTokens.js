ae12fdff9f66fe249004d8103e6b4837df47aa01
Saidamir <your_email@gmail.com>
2026-03-08 15:44:43 +0500

    update user

--- src/utils/generateTokens.js
+++ src/utils/generateTokens.js
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: "1h",
-  });
+  return jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "1h" },
+  );
 };
 
 export const generateRefreshToken = (user) => {
